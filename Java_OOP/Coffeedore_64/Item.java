5e1fbc85a266204323aaa1f9eaff8a0954ac7505
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-10-30 19:14:53 +0200

    ready

---
+++ Java_OOP/Coffeedore_64/Item.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Item {
+    private String name;
+    private double price;
+    private int index;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
